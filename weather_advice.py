6b812a759a2eba498d2fbca15fbcacde488fa364
root <calebmiruni17@gmail.com>
2024-12-22 02:49:17 -0800

    Weather Recommendation Program

--- weather_advice.py
+++ weather_advice.py
@@ -1,4 +1,4 @@
-weather = input("what is the weather like today? (sunny, rainy, cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
@@ -7,4 +7,3 @@ elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
-
